7c1d1ab00668cdf5810b02b0cff3a5d1a090aef5
shubhambodake04 <shubhambodake6476@gmail.com>
2021-05-14 22:11:07 +0530

    Fefactor code

--- FileIODemo/Program.cs
+++ FileIODemo/Program.cs
@@ -5,22 +5,25 @@ namespace FileIODemo
 {
     class Program
     {
+        const string filepath = @"C:\Users\Shubham\source\FileIODemo\FileIODemo\Example.txt";
+        const string copypath = @"C:\Users\Shubham\source\FileIODemo\FileIODemo\ExampleNew.txt";
         static void Main(string[] args)
         {
+           
             //FileExists();
             //ReadAllLines();
             //ReadAllText();
             //FileCopy();
             //DeleteFile();
             //ReadFromStreamReader();
-            WriteUsingStreamWriter();
+            //WriteUsingStreamWriter();
             Console.ReadLine();
         }
 
         public static void FileExists()
         {
-            String path = @"C:\Users\Shubham\source\FileIODemo\FileIODemo\Example.txt";
-            if (File.Exists(path))
+           
+            if (File.Exists(filepath))
                 Console.WriteLine("File exixts");
             else
                 Console.WriteLine("File doesn't exists");
@@ -28,40 +31,35 @@ namespace FileIODemo
 
         public static void ReadAllLines()
         {
-            String path = @"C:\Users\Shubham\source\FileIODemo\FileIODemo\Example.txt";
+            
             String[] lines;
 
-            lines = File.ReadAllLines(path);
+            lines = File.ReadAllLines(filepath);
             Console.WriteLine(lines[0]);
             Console.WriteLine(lines[1]);
         }
         public static void ReadAllText()
         {
-            string path = @"C:\Users\Shubham\source\FileIODemo\FileIODemo\Example.txt";
+            
             string lines;
 
-            lines = File.ReadAllText(path);
+            lines = File.ReadAllText(filepath);
             Console.WriteLine(lines);
         }
         public static void FileCopy()
         {
-            String path = @"C:\Users\Shubham\source\FileIODemo\FileIODemo\Example.txt";
-            String copypath = @"C:\Users\Shubham\source\FileIODemo\FileIODemo\ExampleNew.txt";
-
-            File.Copy(path, copypath);
+            File.Copy(filepath, copypath);
         }
         public static void DeleteFile()
         {
-            String path = @"C:\Users\Shubham\source\FileIODemo\FileIODemo\ExampleNew.txt";
-
-
-            File.Delete(path);
+            
+            File.Delete(filepath);
 
         }
         public static void ReadFromStreamReader()
         {
-            String path = @"C:\Users\Shubham\source\FileIODemo\FileIODemo\Example.txt";
-            using (StreamReader sr=File.OpenText(path))
+            
+            using (StreamReader sr=File.OpenText(copypath))
             {
                 String s = "";
                 while ((s = sr.ReadLine()) !=null)
@@ -74,12 +72,12 @@ namespace FileIODemo
 
         public static void WriteUsingStreamWriter()
         {
-            String path = @"C:\Users\Shubham\source\FileIODemo\FileIODemo\Example.txt";
-            using (StreamWriter sr = File.AppendText(path))
+            
+            using (StreamWriter sr = File.AppendText(copypath))
             {
                 sr.WriteLine("Hello world - .Net is awesome");
                 sr.Close();
-                Console.WriteLine(File.ReadAllText(path));
+                Console.WriteLine(File.ReadAllText(copypath));
             }
         }
     }
